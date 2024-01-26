b1a369d9c218e184f335d557e20e8bb9f1e9a76b
blizz <88501734+G-J-S-B@users.noreply.github.com>
2024-01-26 15:37:09 -0500

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,19 +1,92 @@
+var wordsThusFar = []
 var guessWord = []
+var missPattern = []
+var answer = []
+var gameDifficulty = 'hard'
+var word = getWord();
 
-$('body').on('keydown', function(e){
-    console.log(e.key);
-    if (e.key === 'enter'){
-        var word = $('#guess-word').val();
-        $('#guess-word').val(word);
-        letterSplit(word);
-    }
-    else{
-    }
-});
 
-function letterSplit(e){
-    var word = e;
-    for (var i = 0; i < word.length; i++) {
-    guessWord = (word.charAt(i));
+
+function startProgram(){
+
+    wordsThusFar.push(word);
+
+    displayWord(word);
+    letterSplit();  
+    difficulty(gameDifficulty);
+    setLetters();
+}
+ 
+function getWord(){
+    var url = window.location.search.substring(1)
+    var match = /word=([^&]*)/.exec(url);
+    var word = match ? decodeURIComponent(match[1]) : "";
+    displayWord(word);
+    return word;
+}
+
+function displayWord(x){
+        $('#guess-word').val(x);
+}
+
+function letterSplit(){
+    for (var i = 0; i < word.length ; i++){
+    guessWord.push((word.charAt(i)));
+    console.log('guessWord : ' +  guessWord);
     }
+}
+
+function difficulty(gameDifficulty){
+    if (gameDifficulty == 'easy'){
+        var letterToMiss = Math.floor((20/100) * word.length);
+        pattern(letterToMiss);
+    }
+    else if (gameDifficulty == 'medium'){
+        var letterToMiss = Math.floor((40/100) * word.length);
+        pattern(letterToMiss);
+    }
+    else if (gameDifficulty == 'hard' ){
+        var letterToMiss = Math.floor((80/100) * word.length);
+        pattern(letterToMiss);
+    }
+}
+
+function pattern (lettersToMiss){
+    for(var i = 0; i < word.length; i++){
+        var randomNumber = Math.floor(2 * Math.random())
+        x = guessWord[i];
+        if (randomNumber == 0 && lettersToMiss != 0 ){
+            missPattern.push(0);
+            lettersToMiss = lettersToMiss - 1;
+        }
+        else if (randomNumber == 1 && lettersToMiss != 0 ){
+            missPattern.push(x);
+        }
+        else if (randomNumber == 0 && lettersToMiss == 0 ){
+            missPattern.push(0);
+            lettersToMiss = lettersToMiss - 1;
+        }
+        else if (randomNumber == 1 && lettersToMiss == 0  ){
+            missPattern.push(x);
+        }
+    }
+}     
+
+function setLetters(){
+    for (var i = 0; i < word.length; i++){
+        var x = missPattern[i];
+        if( x === 0 ){
+            $('#set-word').append($('<input type="text" id="' + i + '" class="letter-block" maxlength="1">'));
+            $('#set-word #' + i).val('');
+
+        }
+        else if( x != 0 ){
+            $('#set-word').append($('<div id="' + i + '" class="letter-block">'));
+            $('#set-word #' + i).html(missPattern[i]);
+        }
+    }       
+}
+
+function SaviorGuess(){
+
 }
